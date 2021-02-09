87e460663af72dfc99b85f8c852ca1ce8899604a
pachofisico <pachofisico@gmail.com>
2021-02-08 21:09:48 -0500

    Primera version de instaladores

---
+++ installcentral.sh
@@ -0,0 +1,80 @@
+
+# http://github.com/nordic-institute/X-Road/blob/develop/doc/Manuals/ig-cs_x-road_6_central_server_installation_guide.md
+
+# Preparar el sistema operativo
+# Agregar usuario y asignar clave
+sudo adduser central
+#sudo useradd central
+
+
+#Introducir clave luego de esta instruccion
+#sudo passwd central
+
+
+# Establecer la configuración regional del sistema operativo
+# Agregar la siguiente linea al archivo de entorno: LC_ALL=en_US.UTF-8
+ sudo nano /etc/environment
+LC_ALL=en_US.UTF-8
+
+
+
+# Agregar carpeta con archivos compilados al archivo del repositorio de UBUNTU.  Poner siguiente linea al inicio:
+# deb [trusted=yes] file:///home/ubuntu/ubuntu18.04
+sudo nano /etc/apt/sources.list 
+
+
+
+# Descargar compilados
+wget https://github.com/andcolombia/XROAD-CO/releases/download/6.24.1-CO-Ubuntu/ubuntu18.04.zip
+
+# Descomprimir compilados
+unzip compilados-20201224T152509Z-001.zip
+
+# Ingresar a carpeta de compilados
+cd ubuntu18.04
+
+# Leer compilados
+sudo apt-get install dpkg-dev
+
+
+
+sudo apt install acl curl apt-transport-https locales aptitude software-properties-common
+
+
+
+
+
+
+
+
+
+# Actualizar
+sudo apt-get update
+
+#  Asegurarse de que los paquetes locales y software-properties-common estan presentes
+sudo apt-get install locales software-properties-common
+
+
+
+# Instalar los paquetes de posgresql (Cliente y Servidor)
+#sudo apt install postgresql postgresql-contrib
+
+
+
+# Agregar repositorio de paquetes Java
+curl https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+sudo apt-add-repository -y "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb $(lsb_release -sc) main"
+
+
+# Instalar JRE y fijarlo como la version de Java JDK 8 por defecto
+sudo apt install adoptopenjdk-8-hotspot-jre
+sudo update-java-alternatives -s adoptopenjdk-8-hotspot-jre-amd64
+
+
+# Actualizar nuevamente, antes de instalar
+sudo apt-get update
+
+# Instalar el servidor central
+sudo apt-get install xroad-centralserver
+#sudo apt install xroad-centralserver 
+
---
+++ installxroad.sh
@@ -0,0 +1,79 @@
+# https://x-tee.ee/docs/test/xroad/ig-ss_x-road_v6_security_server_installation_guide.html
+
+
+# Preparar el sistema operativo
+# Agregar usuario y asignar clave
+sudo adduser seguridad
+#sudo useradd seguridad
+
+
+#Introducir clave luego de esta instruccion
+#sudo passwd seguridad
+
+
+# Establecer la configuración regional de idioma en el ambiente del sistema operativo
+# Agregar la siguiente linea al archivo de entorno: LC_ALL=en_US.UTF-8
+ sudo nano /etc/environment
+LC_ALL=en_US.UTF-8
+
+# Agregar carpeta con archivos compilados al archivo del repositorio de UBUNTU.  Poner siguiente linea al inicio:
+# deb [trusted=yes] file:///home/ubuntu/ubuntu18.04
+sudo nano /etc/apt/sources.list 
+
+
+
+
+# Descargar compilados
+wget https://github.com/andcolombia/XROAD-CO/releases/download/6.24.1-CO-Ubuntu/ubuntu18.04.zip
+
+# Descomprimir compilados
+unzip compilados-20201224T152509Z-001.zip
+
+# Ingresar a carpeta de compilados
+cd ubuntu18.04
+
+# Leer compilados
+sudo apt-get install dpkg-dev
+
+# Leer compilados y crear resumen
+dpkg-scanpackages . >Packages
+
+sudo apt install acl curl apt-transport-https locales aptitude software-properties-common
+
+
+
+
+
+
+#  Asegurarse de que los paquetes locales y software-properties-common estan presentes
+sudo apt-get install locales software-properties-common
+sudo locale-gen en_US.UTF-8
+
+
+# Asegurarse de que la zona horaria es correcta
+timedatectl
+
+# Si la zona horaria no es correcta, actualizarla desde la lista
+timedatectl list-timezones
+
+
+
+# Actualizar
+sudo apt-get update
+
+
+
+# Instalar el servidor de seguridad
+sudo apt-get install xroad-securityserver 
+# sudo apt install xroad-securityserver 
+
+
+
+
+-----------------------------------
+
+sudo apt install postgresql postgresql-contrib
+sudo apt-get update
+sudo useradd seguridad
+sudo passwd seguridad
+
